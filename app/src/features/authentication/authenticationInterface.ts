5a5c9aded63b7daeaa630b7a8758e8c5fc958590
Mattias <mattiaswelamsson@gmail.com>
2023-12-05 11:20:07 +0900

    update authenticationSlice and authguard logic

--- app/src/features/authentication/authenticationInterface.ts
+++ app/src/features/authentication/authenticationInterface.ts
@@ -1,48 +1,52 @@
+import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query';
+
 export interface Company {
-  id: number | null;
-  name: string | null;
+	id: number | null;
+	name: string | null;
 }
 export interface AuthState {
-  status: "IDLE" | "LOADING" | "AUTHENTICATED" | "FAILED";
-  isAuthenticated: boolean;
-  user: {
-    username: string | null;
-    first_name: string | null;
-    last_name: string | null;
-    email: string | null;
-    profile_picture: string | null;
-    companies: Company[];
-    id: string | null;
-  };
+	loading: boolean;
+	userInfo: {
+		username: string | null;
+		first_name: string | null;
+		last_name: string | null;
+		email: string | null;
+		profile_picture: string | null;
+		companies: Company[];
+		id: string | null;
+	};
+	userToken: string | null;
+	error: FetchBaseQueryError | unknown;
+	success: boolean;
 }
 export interface UserReadSerializer {
-  username: string | null;
-  email: string | null;
-  first_name: string | null;
-  last_name: string | null;
-  profile_picture: string | null;
-  companies: Company[];
-  pk: string | null;
+	username: string | null;
+	email: string | null;
+	first_name: string | null;
+	last_name: string | null;
+	profile_picture: string | null;
+	companies: Company[];
+	pk: string | null;
 }
 
 export interface Credentials {
-  email: string;
-  password: string;
+	email: string;
+	password: string;
 }
 
 export interface AccessToken {
-  access: string | undefined;
+	access: string | undefined;
 }
 
 export interface RefreshToken {
-  refresh: string | undefined;
+	refresh: string | undefined;
 }
 
 export interface Token {
-  access: string | undefined;
-  refresh: string | undefined;
+	access: string | undefined;
+	refresh: string | undefined;
 }
 
 export interface GoogleCode {
-  code: string;
+	code: string;
 }
