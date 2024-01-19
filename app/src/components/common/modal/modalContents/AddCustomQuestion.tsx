74494eace0657891e230b9a3cf4a1d0746433d3e
Muhammad Tahir <mtahir.ps@gmail.com>
2024-01-19 15:42:05 +0500

    feat: update question api call

--- app/src/components/common/modal/modalContents/AddCustomQuestion.tsx
+++ app/src/components/common/modal/modalContents/AddCustomQuestion.tsx
@@ -5,22 +5,28 @@ import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectInterviewDetail } from '@/features/interviewDetail/interviewDetailSlice';
 import { useAddTemplateQuestionMutation } from '@/features/templates/templatesQuestionsAPISlice';
+import { useAddQuestionMutation } from '@/features/questions/questionsAPISlice';
 
 const AddCustomQuestion = () => {
   const { templateId } = useParams();
   const { selectedSection } = useSelector(selectInterviewDetail);
   const [newQuestion] = useAddTemplateQuestionMutation();
+  const [addQuestion] = useAddQuestionMutation();
 
   const templateID = templateId;
   const customQuestionFormRef = useRef(null);
   const handleQuestionCreated = async (question: {}) => {
-    const requestData = {
-      template_id: templateID,
-      topic: String(selectedSection.id),
-      question: question,
-    };
     try {
-      await newQuestion(requestData).unwrap();
+      if (templateId) {
+        const requestData = {
+          template_id: templateID,
+          topic: String(selectedSection.id),
+          question: question,
+        };
+        await newQuestion(requestData).unwrap();
+      } else {
+        await addQuestion(question).unwrap();
+      }
     } catch (error) {
       // Handle error, e.g., display a notification
       console.error('Failed to add question:', error);
